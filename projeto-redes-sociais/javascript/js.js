631c5d9a1de7154bb82452ca20d6b20a1932c391
Caroliny Rocha Sampaio <129858370+Carolinyr9@users.noreply.github.com>
2023-08-18 11:58:23 -0300

    .

--- projeto-redes-sociais/javascript/js.js
+++ projeto-redes-sociais/javascript/js.js
@@ -1,65 +1,16 @@
-class MobileNavbar {
-  constructor(mobileMenu, navList, navLinks) {
-    this.mobileMenu = document.querySelector(mobileMenu);
-    this.navList = document.querySelector(navList);
-    this.navLinks = document.querySelectorAll(navLinks);
-    this.activeClass = "active";
+//referências
+var toggleBtn = document.getElementById('toggleBtn');
+var navigation = document.querySelector('.navigation');
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+// evento de clique para toggleBtn
+toggleBtn.addEventListener('click', function() {
+    // Verifica se a classe "active" já está presente na navigation
+    var isActive = navigation.classList.contains('active');
 
-  animateLinks() {
-    this.navLinks.forEach((link, index) => {
-      link.style.animation
-        ? (link.style.animation = "")
-        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-            index / 7 + 0.3
-          }s`);
-    });
-  }
-
-  handleClick() {
-    this.navList.classList.toggle(this.activeClass);
-    this.mobileMenu.classList.toggle(this.activeClass);
-    this.animateLinks();
-  }
-
-  addClickEvent() {
-    this.mobileMenu.addEventListener("click", this.handleClick);
-  }
-
-  init() {
-    if (this.mobileMenu) {
-      this.addClickEvent();
+    // Se já estiver ativa, remove a classe; caso contrário, adiciona a classe
+    if (isActive) {
+        navigation.classList.remove('active');
+    } else {
+        navigation.classList.add('active');
     }
-    return this;
-  }
-}
-
-const mobileNavbar = new MobileNavbar(
-  ".mobile-menu",
-  ".nav-list",
-  ".nav-list li",
-);
-mobileNavbar.init();
-
-// slides
-
-var radio = document.querySelector('.manual-btn')
-var cont = 1
-
-document.getElementById('radio1').checked = true
-
-setInterval(() => {
-    proximaImg()
-}, 5000)
-
-function proximaImg(){
-    cont++
-
-    if(cont > 3){
-        cont = 1 
-    }
-
-    document.getElementById('radio'+cont).checked = true
-}
+});
